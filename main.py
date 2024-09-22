9e8c3de71150a60e383e79ef066e109f62a67db5
Daniel Castillo <dev@dcastillogi.com>
2024-09-22 14:29:01 -0500

    solution clean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-import joblib
 
 # Load data
 train_data = pd.read_csv('train_dataset.csv')
